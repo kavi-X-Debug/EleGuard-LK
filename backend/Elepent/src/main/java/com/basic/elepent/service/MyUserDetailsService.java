b0017623b9d2af5f4930432607386e2b95406bb8
ravindulakshitha2003 <ravindulakshitha20032411@gmail.com>
2026-05-07 22:40:46 +0530

    main backend uploedrd

---
+++ backend/Elepent/src/main/java/com/basic/elepent/service/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.basic.elepent.service;
+
+
+import com.basic.elepent.entity.FarmerEntity;
+import com.basic.elepent.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        FarmerEntity farmer = userRepository.findByUsername(username).orElse(null);
+        if (farmer == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        UserDetails user = User.builder()
+                .username(farmer.getUsername())
+                .password(farmer.getPassword())
+                .build();
+
+        return user;
+    }
+}
---
+++ backend/Elepent/src/test/java/com/basic/elepent/ElepentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.elepent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElepentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
